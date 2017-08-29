c8691e73c1f0e490eacf600b48f7295a574464b3
Guest User <Guest@Epicodus-9D.local>
2017-08-29 16:24:52 -0700

    made shit work

--- js/scripts.js
+++ js/scripts.js
@@ -127,6 +127,25 @@ $(document).ready(function() {
     var weekendInputted = $("#weekendInput").val();
     weekendDisplay(weekendInputted);
   });
+  $(document).ready(function(){
+    var slideIndex = 0;
+    showSlides();
 
+    function showSlides() {
+        var i;
+        console.log(slideIndex)
+        var slides = document.getElementsByClassName("mySlides");
+        console.log(slides)
+        console.log(slides.length)
+        for (i = 0; i < slides.length; i++) {
+           slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex> slides.length) {slideIndex = 1;
+        }
+        slides[slideIndex-1].style.display = "block";
+        setTimeout(showSlides, 2000); // Change image every 2 seconds
+    }
+
+  });
 });
-
