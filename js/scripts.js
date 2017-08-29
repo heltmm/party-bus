a9d975b244c5ce19b77975c79a3546f060617f99
Mark Helt <heltmm@gmail.com>
2017-08-29 11:34:34 -0700

    fix merge bugs

--- js/scripts.js
+++ js/scripts.js
@@ -54,8 +54,9 @@ function progressBar(){
   '</div>');
   if (count > 11){
     $(".updatedBar").html('<div class="progress">' +
-    '<div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: ' + '%; height: 30px;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>' +
-  '</div>');
+    '<div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" role="progressbar" style="width: 100%; height: 30px;" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">FULL BUSS. LESSSSS GOOOOOO</div>' +
+    '</div>')
+    $("#fire").html('<div id="inALine"><p><img src="img/fire.png" alt=""><img src="img/fire.png" alt=""><img src="img/fire.png" alt=""><img src="img/fire.png" alt=""></p></div>')
   }
   count ++;
 }
