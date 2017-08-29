1e9cfb78246a915a358d94f2b94715c6c21342b4
DanDanilyuk <DanDanilyuk@gmail.com>
2017-08-29 09:34:52 -0700

    add weekendDisplay function

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 
 ///BACK END
-var bar = 0
+var bar = 0;
 var contacts = [];
 
 function Contact(first, last, weekend, number, carrier){
@@ -18,10 +18,10 @@ function Address(street1, city1, state1){
 }
 Contact.prototype.firstAndLast = function () {
   return this.firstName + " "+this.lastName;
-}
+};
 Address.prototype.fullAddress = function () {
   return this.street+", " + this.city+ ", "+this.state;
-}
+};
 function resetFields(){
   $("input#firstName").val("");
   $("input#lastName").val("");
@@ -33,22 +33,28 @@ function resetFields(){
 function progressBar(){
   $(".updatedBar").html('<div class="progress">' +
   '<div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: ' + bar + '%; height: 30px;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>' +
-  '</div>')
+  '</div>');
   if (bar > 99){
     $(".updatedBar").html('<div class="progress">' +
     '<div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" role="progressbar" style="width: 100%; height: 30px;" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">FULL BUSS. LESSSSS GOOOOOO</div>' +
-    '</div>')
-    $("#fire").html('<div id="inALine"><p><img src="img/fire.png" alt=""><img src="img/fire.png" alt=""><img src="img/fire.png" alt=""><img src="img/fire.png" alt=""></p></div>')
+    '</div>');
+    $("#fire").html('<div id="inALine"><p><img src="img/fire.png" alt=""><img src="img/fire.png" alt=""><img src="img/fire.png" alt=""><img src="img/fire.png" alt=""></p></div>');
   }
-  bar += (100/12)
+  bar += (100/12);
 }
-var phoneNumber = parseInt($("#phoneNumber").val());
+var phoneNumber;
 var carrier;
 var message;
 var name;
 var email = phoneNumber + carrier;
 function sendMail() {
   window.location.href = 'mailto:' + email + '?subject=Ski Confirmation' + '&body=' + message;
+}
+function weekendDisplay(weekendInputted){
+  if (weekendInputted === "11/22") {
+    alert("11/22 Selected");
+    $("#trip-info").show();
+  } return;
 }
 
 /////FRONT END
@@ -59,10 +65,11 @@ $(document).ready(function() {
     var firstNameInputted = $("#firstName").val();
     var lastNameInputted = $("#lastName").val();
     var weekendInputted = $("#weekendInput").val();
+    weekendDisplay(weekendInputted);
     var phoneNumber = parseInt($("#phoneNumber").val());
     var carrier = $("#carrier").val();
     var newContact = new Contact(firstNameInputted, lastNameInputted, weekendInputted, phoneNumber, carrier);
-    alert(carrier)
+    alert(carrier);
     console.log(newContact);
 
     $(".new-address").each(function (){
@@ -83,9 +90,6 @@ $(document).ready(function() {
       progressBar();
       resetFields();
       //sendMail();
-
     });
-
   });
-
 });
